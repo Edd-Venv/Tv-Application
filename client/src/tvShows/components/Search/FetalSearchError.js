c6e44737f8313a8fc1b67dd5b087f9b53b95f283
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-04-04 11:28:36 +0200

    Update

--- client/src/tvShows/components/Search/FetalSearchError.js
+++ client/src/tvShows/components/Search/FetalSearchError.js
@@ -1,18 +1,16 @@
 import React from "react";
 import "./Search.css";
 
-class FetalSearchError extends React.Component {
-  render() {
-    const { handleClick } = this.props;
-    return (
-      <React.Fragment>
-        <span onClick={handleClick} className="close" id="error-close-button">
-          ×
-        </span>
-        <p id="error-paragraph">Sorry Show Not In DataBase</p>
-      </React.Fragment>
-    );
-  }
-}
+const FetalSearchError = props => {
+  const { handleClose } = props;
+  return (
+    <React.Fragment>
+      <span onClick={handleClose} className="close" id="error-close-button">
+        ×
+      </span>
+      <p id="error-paragraph">Sorry Show Not In DataBase</p>
+    </React.Fragment>
+  );
+};
 
 export default FetalSearchError;
