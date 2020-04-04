7c4f0cb3e0d6a40c5b97da77e4a256cf4206d263
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-04-04 11:28:14 +0200

    Update

---
+++ client/src/tvShows/components/Search/DummyState.js
@@ -0,0 +1,30 @@
+export const initialState = {
+  isLoaded: false,
+  data: {
+    id: 4296,
+    url: "http://www.tvmaze.com/shows/4296/the-tom-and-jerry-show",
+    name: "The Tom and Jerry Show",
+    type: "Animation",
+    language: "English",
+    genres: (2)[("Comedy", "Children")],
+    status: "Running",
+    runtime: 30,
+    premiered: "2014-04-09",
+    officialSite: "http://www.cartoonnetwork.com/video/tomjerry/episodes/",
+    schedule: { time: "09:00", days: Array(1) },
+    rating: { average: null },
+    weight: 0,
+    network: { id: 11, name: "Cartoon Network" },
+    webChannel: null,
+    externals: { tvrage: 41747, thetvdb: 280446, imdb: "tt3559124" },
+    image: {
+      original: ""
+    },
+    summary: "<p>The famous cat and mouse duo is back in an all-new series",
+    updated: 1573583964,
+    _links: { self: { href: "http://api.tvmaze.com/shows/4296" } }
+  },
+  display: "hide",
+  Test: "show",
+  showTrailer: ""
+};
--- client/src/tvShowsApp.js
+++ client/src/tvShowsApp.js
@@ -2,23 +2,32 @@ import React from "react";
 import Header from "./tvShows/components/Header/Header";
 import { TvContextProvider } from "./tvShows/contexts/tvContext.js";
 import Body from "./tvShows/components/Body/Body";
+import Search from "./tvShows/components/Search/Search";
+import Slider from "./tvShows/components/Slider/Slider.js";
+
 function TvShows() {
   return (
-    <TvContextProvider>
-      <h2
-        style={{
-          textAlign: "center",
-          fontSize: "1.5em",
-          fontWeight: "bolder",
-          color: "white"
-        }}
-      >
-        TRENDING SHOWS
-      </h2>
-      <Header />
-      <br />
-      <Body />
-    </TvContextProvider>
+    <React.Fragment>
+      <TvContextProvider>
+        <h2
+          style={{
+            textAlign: "center",
+            fontSize: "1.5em",
+            fontWeight: "bolder",
+            color: "white"
+          }}
+        >
+          TRENDING SHOWS
+        </h2>
+        <Header />
+        <br />
+        <Body />
+        <br />
+        <Search />
+        <br />
+        <Slider />
+      </TvContextProvider>
+    </React.Fragment>
   );
 }
 export default TvShows;
