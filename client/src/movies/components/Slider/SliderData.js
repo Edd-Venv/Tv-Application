20f3675c9886b18e2ea1bb6cd249a2c839772c61
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-04-07 20:37:35 +0200

    update

---
+++ client/src/movies/components/Slider/SliderData.js
@@ -0,0 +1,111 @@
+const recommedations = [
+  {
+    Name: "The Hunger Games",
+    Type: "movie",
+    wTeaser:
+      "The Hunger Games is a 2012 American dystopian science fiction-adventure film directed by Gary Ross and based on Suzanne Collins’s 2008 novel of the same name. It is the first installment in The Hunger Games film series and was produced by Nina Jacobson and Jon Kilik, with a screenplay by Ross, Collins, and Billy Ray. The film stars Jennifer Lawrence, Josh Hutcherson, Liam Hemsworth, Woody Harrelson, Elizabeth Banks, Lenny Kravitz, Stanley Tucci, and Donald Sutherland. The story takes place in a dystopian post-apocalyptic future in the nation of Panem, where a boy and a girl from each of the nation's 12 Districts are chosen annually as tributes and forced to compete in The Hunger Games, an elaborate televised fight to the death. Katniss Everdeen (Jennifer Lawrence) volunteers to take her younger sister's place, and with her district's male tribute, Peeta Mellark (Josh Hutcherson), travels to the Capitol to train and compete in the games.",
+    wUrl: "http://en.wikipedia.org/wiki/The_Hunger_Games_(film)",
+    yUrl: "https://www.youtube-nocookie.com/embed/mfmrPu43DF8",
+    yID: "mfmrPu43DF8",
+  },
+
+  {
+    Name: "The Chronicles Of Narnia: The Lion, The Witch And The Wardrobe",
+    Type: "movie",
+    wTeaser:
+      "The Chronicles of Narnia: The Lion, the Witch and the Wardrobe is a 2005 fantasy film co-written and directed by Andrew Adamson, based on The Lion, the Witch and the Wardrobe, the first published and second chronological novel in C. S. Lewis's children's epic fantasy series, The Chronicles of Narnia. It was co-produced by Walden Media and Walt Disney Pictures and distributed by Buena Vista Pictures. William Moseley, Anna Popplewell, Skandar Keynes and Georgie Henley play Peter, Susan, Edmund, and Lucy, four British children evacuated during the Blitz to the countryside, who find a wardrobe that leads to the fantasy world of Narnia. There they ally with the lion Aslan (voiced by Liam Neeson) against the forces of Jadis, the White Witch (Tilda Swinton).",
+    wUrl:
+      "http://en.wikipedia.org/wiki/The_Chronicles_of_Narnia:_The_Lion,_the_Witch_and_the_Wardrobe",
+    yUrl: "https://www.youtube-nocookie.com/embed/LshOd31d-yE",
+    yID: "LshOd31d-yE",
+  },
+
+  {
+    Name: "2012",
+    Type: "movie",
+    wTeaser:
+      "2012 is a 2009 American disaster film co-written and directed by Roland Emmerich. The film was produced by Harald Kloser, Mark Gordon and Larry J. Franco. Kloser wrote the screenplay with Emmerich and the film was distributed by Columbia Pictures and produced by Emmerich's Centropolis Entertainment. The plot follows geologist Adrian Helmsley, who discovers the earth's crust is becoming unstable after a massive solar flare caused by an alignment of the planets, and novelist Jackson Curtis as he attempts to bring his family to safety as the world is destroyed by a series of extreme natural disasters caused by this. The film refers to Mayanism and the 2012 phenomenon in its portrayal of cataclysmic events. Filming, originally planned for Los Angeles, began in Vancouver in August 2008.",
+    wUrl: "http://en.wikipedia.org/wiki/2012_(film)",
+    yUrl: "https://www.youtube-nocookie.com/embed/ce0N3TEcFw0",
+    yID: "ce0N3TEcFw0",
+  },
+
+  {
+    Name: "Titanic",
+    Type: "movie",
+    wTeaser:
+      " Titanic is a 1997 American epic romance and disaster film directed, written, co-produced, and co-edited by James Cameron. Incorporating both historical and fictionalized aspects, the film is based on accounts of the sinking of the RMS Titanic, and stars Leonardo DiCaprio and Kate Winslet as members of different social classes who fall in love aboard the ship during its ill-fated maiden voyage.Cameron's inspiration for the film came from his fascination with shipwrecks; he felt a love story interspersed with the human loss would be essential to convey the emotional impact of the disaster. Production began in 1995, when Cameron shot footage of the actual Titanic wreck. The modern scenes on the research vessel were shot on board the Akademik Mstislav Keldysh, which Cameron had used as a base when filming the wreck. Scale models, computer-generated imagery, and a reconstruction of the Titanic built at Baja Studios were used to re-create the sinking. The film was co-financed by Paramount Pictures and 20th Century Fox; the former handled distribution in North America while the latter released the film internationally. It was the most expensive film ever made at the time, with a production budget of $200 million.",
+    wUrl: "http://en.wikipedia.org/wiki/Titanic_(1997_film)",
+    yUrl: "https://www.youtube-nocookie.com/embed/ezcvpLIyifU",
+    yID: "ezcvpLIyifU",
+  },
+
+  {
+    Name: "Harry Potter And The Deathly Hallows – Part 2",
+    Type: "movie",
+    wTeaser:
+      "Harry Potter and the Deathly Hallows – Part 2 is a 2011 fantasy film directed by David Yates and distributed by Warner Bros. Pictures. It is the second of two cinematic parts based on J. K. Rowling's 2007 novel of the same name. The film, which is the eighth and final instalment in the Harry Potter series, was written by Steve Kloves and produced by David Heyman, David Barron, and Rowling. The story continues to follow Harry Potter's quest to find and destroy Lord Voldemort's Horcruxes in order to stop him once and for all.The film stars Daniel Radcliffe as Harry Potter, alongside Rupert Grint and Emma Watson as Harry's best friends, Ron Weasley and Hermione Granger. Principal photography began on 19 February 2009, and was completed on 12 June 2010, with reshoots taking place in December 2010, more than ten years after filming started on the first instalment of the series. Part 2 was released in 2D, 3-D and IMAX cinemas worldwide from 13–15 July 2011, and is the only Harry Potter film to be released in 3-D.",
+    wUrl:
+      "http://en.wikipedia.org/wiki/Harry_Potter_and_the_Deathly_Hallows_%E2%80%93_Part_2",
+    yUrl: "https://www.youtube-nocookie.com/embed/mObK5XD8udk",
+    yID: "mObK5XD8udk",
+  },
+  {
+    Name: "New Moon",
+    Type: "movie",
+    wTeaser:
+      "The Twilight Saga: New Moon (commonly referred to as New Moon) is a 2009 American romantic fantasy film based on Stephenie Meyer's 2006 novel New Moon. It is the second film in The Twilight Saga film series and is the sequel to 2008's Twilight. Summit Entertainment greenlit the sequel in late November 2008, following the early success of Twilight. Directed by Chris Weitz, the film stars Kristen Stewart, Robert Pattinson, and Taylor Lautner, reprising their roles as Bella Swan, Edward Cullen, and Jacob Black, respectively. Melissa Rosenberg, who handed in a draft of the film script during the opening weekend of Twilight, returned as screenwriter for New Moon as well.Filming began in Vancouver in late March 2009, and in Montepulciano, Italy in late May 2009. The film was released on November 20, 2009 in most countries, and set domestic box office records as the biggest midnight screening, grossing $26.3 million, which was superseded by its sequel, Eclipse. This led to the highest single-day, domestic gross on an opening day, with $72.7 million, until it was beaten in 2011 by Harry Potter and the Deathly Hallows – Part 2, which made $91.1 million, by nearly $20 million. Furthermore, New Moon opened with the third-highest domestic opening weekend since 2002 grossing a total of $142,839,137. The film also became the highest-grossing film released by Summit Entertainment, and was the widest independent release, playing in over 4,100 theaters in its theatrical run, until it was surpassed by The Twilight Saga: Eclipse.",
+    wUrl: "http://en.wikipedia.org/wiki/The_Twilight_Saga:_New_Moon",
+    yUrl: "https://www.youtube-nocookie.com/embed/U6dNuMGaV2k",
+    yID: "U6dNuMGaV2k",
+  },
+
+  {
+    Name: "The Lion King",
+    Type: "movie",
+    wTeaser:
+      "The Lion King is a 1994 American animated musical film produced by Walt Disney Feature Animation and released by Walt Disney Pictures. It is the 32nd Disney animated feature film, and the fifth animated film produced during a period known as the Disney Renaissance. The Lion King was directed by Roger Allers and Rob Minkoff, produced by Don Hahn, and has a screenplay credited to Irene Mecchi, Jonathan Roberts, and Linda Woolverton. Its original songs were written by composer Elton John and lyricist Tim Rice, with a score by Hans Zimmer. The film features an ensemble voice cast that includes Matthew Broderick, James Earl Jones, Jeremy Irons, Jonathan Taylor Thomas, Moira Kelly, Nathan Lane, Ernie Sabella, Rowan Atkinson, Robert Guillaume, Madge Sinclair (in her last film role), Whoopi Goldberg, Cheech Marin, and Jim Cummings. The story takes place in a kingdom of lions in Africa and was influenced by the Biblical stories of Joseph and Moses, and William Shakespeare's Hamlet.",
+    wUrl: "https://en.wikipedia.org/wiki/The_Lion_King",
+    yUrl: "https://www.youtube-nocookie.com/embed/7TavVZMewpY",
+    yID: "7TavVZMewpY",
+  },
+
+  {
+    Name: "Avatar",
+    Type: "movie",
+    wTeaser:
+      "Avatar (marketed as James Cameron's Avatar) is a 2009 American epic science fiction film directed, written, produced, and co-edited by James Cameron and stars Sam Worthington, Zoe Saldana, Stephen Lang, Michelle Rodriguez, and Sigourney Weaver. The film is set in the mid-22nd century when humans are colonizing Pandora, a lush habitable moon of a gas giant in the Alpha Centauri star system, in order to mine the mineral unobtanium, a room-temperature superconductor. The expansion of the mining colony threatens the continued existence of a local tribe of Na'vi – a humanoid species indigenous to Pandora. The film's title refers to a genetically engineered Na'vi body operated from the brain of a remotely located human that is used to interact with the natives of Pandora.",
+    wUrl: "http://en.wikipedia.org/wiki/Avatar_(2009_film)",
+    yUrl: "https://www.youtube-nocookie.com/embed/5PSNL1qE6VY",
+    yID: "5PSNL1qE6VY",
+  },
+
+  {
+    Name: "The Karate Kid",
+    Type: "movie",
+    wTeaser:
+      "The Karate Kid is a 1984 American martial arts drama film written by Robert Mark Kamen and directed by John G. Avildsen. It is the first installment in The Karate Kid franchise, and stars Ralph Macchio, Pat Morita, and Elisabeth Shue. The Karate Kid follows Daniel LaRusso (Macchio), a teenager taught Gōjū-ryū karate by Mr. Miyagi (Morita) to help defend himself and compete in a tournament against his bullies, one of which is the ex-boyfriend of his love interest Ali Mills (Shue).Kamen was approached by Columbia Pictures to compose a film similar to Avildsen's previous success Rocky (1976), after signing the director. Kamen drew inspiration from his own life when writing the film.  As a result, he maintained strong opinions regarding cast, and petitioned heavily for Morita's inclusion. Preparations for the film began immediately after the final edit of the script was complete, and casting took place between April and June 1983. Principal photography began on October 31, 1983 in Los Angeles, and filming was complete by December 16, 1983.",
+    wUrl: "https://en.wikipedia.org/wiki/The_Karate_Kid",
+    yUrl: "https://www.youtube-nocookie.com/embed/XY8amUImEu0",
+    yID: "XY8amUImEu0",
+  },
+  {
+    Name: "Breaking Dawn",
+    Type: "movie",
+    wTeaser:
+      "Breaking Dawn is a 2004 American independent horror-thriller film written and directed by Mark Edwin Robinson. It is the directorial debut of Robinson, who was 22 at the time and saw the film showcased at the Cannes Film Festival, Marché du Film and at The Hollywood Film Festival.  The film stars Kelly Overton and James Haven.Dawn, a young medical student is charged with uncovering the murder of a mental patient's mother. The patient, Don, holds many secrets and disturbs Dawn with his insane ramblings. As Dawn continues to investigate the murder, she believes Don's paranoia is out of control at the mention of a menacing figure named Malachi. She begins to question whether Malachi exists when she is stalked by a mysterious figure or if her imagination and Don's craziness are affecting her judgement.",
+    wUrl: "https://en.wikipedia.org/wiki/Breaking_Dawn_(2004_film)",
+    yUrl: "https://www.youtube-nocookie.com/embed/iRib-YC3t0o",
+    yID: "iRib-YC3t0o",
+  },
+  {
+    Name: "Fast & Furious",
+    Type: "movie",
+    wTeaser:
+      "Fast & Furious (originally The Fast and the Furious) is an American media franchise centered on a series of action films that are largely concerned with illegal street racing, heists and spies. The franchise also includes short films, a television series, live shows, video games and theme park attractions. It is distributed by Universal Pictures.The first film was released in 2001, which began the original trilogy of films focused on racing, and culminated in the standalone film The Fast and the Furious: Tokyo Drift (2006). The series then went under a soft reboot with Fast & Furious (2009), which transitioned the series toward heists and spying, and was followed by four sequels. Another sequel is set to be released in 2021, with a tenth and final film also planned. The main films are collectively known as The Fast Saga.",
+    wUrl: "https://en.wikipedia.org/wiki/The_Fast_and_the_Furious",
+    yUrl: "https://www.youtube-nocookie.com/embed/k98tBkRsGl4",
+    yID: "k98tBkRsGl4",
+  },
+];
+export default recommedations;
