c75f09f58fea73982bdbb9ddd6be24b6552157b4
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-04-08 18:32:31 +0200

    Update

--- client/src/tvShows/components/Header/Header.js
+++ client/src/tvShows/components/Header/Header.js
@@ -1,10 +1,28 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { TvContext } from "../../contexts/tvContext";
+import HeaderCard from "./HeaderCard.js";
+import { dummyData } from "../DummyData.js";
 import "./Header.css";
 
 function Header() {
   const { isLoaded, data } = useContext(TvContext);
+  const [state, setState] = useState({
+    data: { ...dummyData },
+  });
 
+  const handleClose = () => {
+    setState({ data: { ...dummyData } });
+    document.getElementById("header-card").style.display = "none";
+    document.getElementById("slider").style.display = "block";
+  };
+
+  const showMainCard = (data) => {
+    if (document.getElementById("header-card") !== null) {
+      setState({ data: data });
+      document.getElementById("slider").style.display = "none";
+      document.getElementById("header-card").style.display = "block";
+    }
+  };
   return (
     <div className="flex-container">
       {isLoaded === false ? (
@@ -18,61 +36,46 @@ function Header() {
       ) : (
         <React.Fragment>
           <div className="header1" key={data[49].id}>
-            <a
-              href={data[49].officialSite}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <button id="button" onClick={showMainCard.bind(this, data[49])}>
               <img
                 alt="loading"
                 src={data[49].image.original}
                 className="img-thumbnail"
                 id="box"
               />
-            </a>
+            </button>
           </div>
           <div className="header2" key={data[6].id}>
-            <a
-              href={data[6].officialSite}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <button id="button" onClick={showMainCard.bind(this, data[6])}>
               <img
                 alt="loading"
                 src={data[6].image.original}
                 className="img-thumbnail"
                 id="box"
               />
-            </a>
+            </button>
           </div>
           <div className="header3" key={data[52].id}>
-            <a
-              href={data[52].officialSite}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <button id="button" onClick={showMainCard.bind(this, data[52])}>
               <img
                 alt="loading"
                 src={data[52].image.original}
                 className="img-thumbnail"
                 id="box"
               />
-            </a>
+            </button>
           </div>
           <div className="header4" key={data[7].id}>
-            <a
-              href={data[7].officialSite}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <button id="button" onClick={showMainCard.bind(this, data[7])}>
               <img
                 alt="loading"
                 src={data[7].image.original}
                 className="img-thumbnail"
                 id="box"
               />
-            </a>
+            </button>
           </div>
+          <HeaderCard data={state.data} handleClose={handleClose} />
         </React.Fragment>
       )}
     </div>
---
+++ client/src/tvShows/components/Header/HeaderCard.js
@@ -0,0 +1,127 @@
+import React, { useContext, useState } from "react";
+import { UserContext } from "../../../App.js";
+import "./HeaderCard.css";
+
+const HeaderCard = (props) => {
+  const [user] = useContext(UserContext);
+  const [state, setState] = useState({ message: "" });
+  const { data, handleClose } = props;
+
+  async function saveShow(Args) {
+    if (!user.accesstoken) setState({ messsage: "You need to login to Save." });
+    else {
+      const result = await (
+        await fetch("http://localhost:4010/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${user.accesstoken}`,
+          },
+          body: JSON.stringify({
+            show_key: Args[0],
+            show_title: Args[1],
+            show_runtime: Args[2],
+            show_status: Args[3],
+            show_premiered: Args[4],
+            show_genre: Args[5],
+            show_rating: Args[6],
+            show_summary: Args[7],
+            show_image: Args[8],
+          }),
+        })
+      ).json();
+
+      if (!result.error) {
+        setState({ message: result.message });
+      } else {
+        setState({ message: result.error });
+      }
+    }
+  }
+
+  return (
+    <div id="header-card">
+      <div className="header-card-container">
+        <div className="card mb-3">
+          <span onClick={handleClose} className="close" id="close-button">
+            ×
+          </span>
+          <div className="row no-gutters">
+            <div className="col-md-4">
+              <div className="header-show-image">
+                <img
+                  src={data.image.original}
+                  alt={data.name}
+                  className="img-thumbnail"
+                />
+              </div>
+            </div>
+            <div className="col-md-8">
+              <div className="card-body" id="header-card-font">
+                <h2 className="card-title">
+                  {data.name}
+                  <hr />
+                </h2>
+
+                <div className="card-text">
+                  <p>
+                    {" "}
+                    <strong>Description:</strong>
+                    {data.summary
+                      .replace(/<p>/g, " ")
+                      .replace(/<b>/g, " ")
+                      .replace(/p>/g, " ")
+                      .replace(/</g, " ")
+                      .replace(/i>/g, " ")
+                      .replace(/b>/g, " ")
+                      .substring(0, 250)}
+                    .
+                  </p>
+                  <p>
+                    <strong>Premiered: </strong>
+                    {data.premiered}{" "}
+                  </p>
+                  <p>
+                    <strong>Runtime: </strong>
+                    {data.runtime} mins
+                  </p>
+                  <p>
+                    <strong>Rating: </strong>
+                    {data.rating.average}
+                  </p>
+
+                  <button
+                    className="btn btn-primary"
+                    onClick={saveShow.bind(this, [
+                      data.id,
+                      data.name,
+                      data.runtime,
+                      data.status,
+                      data.premiered,
+                      data.genres[0],
+                      data.rating.average,
+                      data.summary
+                        .replace(/<p>/g, " ")
+                        .replace(/<b>/g, " ")
+                        .replace(/p>/g, " ")
+                        .replace(/</g, " ")
+                        .replace(/i>/g, " ")
+                        .replace(/b>/g, " ")
+                        .substring(0, 100),
+                      data.image.original,
+                    ])}
+                  >
+                    save
+                  </button>
+                  {state.message}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderCard;
