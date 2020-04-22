1f2233cbbadf2f13cd27c2cd7dba66289b8b2476
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-04-22 12:20:05 +0200

    Update

--- server/database-connection/db.js
+++ server/database-connection/db.js
@@ -8,3 +8,25 @@ const pool = new Pool({
   database: "db",
 });
 module.exports = pool;
+/*
+ user: "postgres",
+  password: "050993Aurum",
+  host: "booksdb.cu7acqeogeq9.us-east-2.rds.amazonaws.com",
+  port: 5432,
+  database: "booksdb"
+
+  "http://18.222.115.53:4000"
+
+  const path = require("path");
+const http = require("http");
+
+  if (true) {
+  server.use(express.static("build"));
+  server.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
+}
+
+http.createServer(server).listen(4000);
+
+*/
