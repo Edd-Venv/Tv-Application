8f42fe489b3a926cd6116629e8773919d60ea47a
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-04-27 19:00:59 +0200

    Update

--- client/src/tvShows/components/Header/HeaderCard.js
+++ client/src/tvShows/components/Header/HeaderCard.js
@@ -56,19 +56,31 @@ const HeaderCard = (props) => {
   return (
     <div id="header-card">
       <div className="header-card-container">
-        <div className="card mb-3">
-          <span onClick={handleClose} className="close" id="close-button">
-            ×
+        <div
+          className="header-card card mb-3"
+          style={{
+            backgroundImage: `url(${data.image.original})`,
+          }}
+        >
+          <span onClick={handleClose} className="close-card-button">
+            <svg
+              className="bi bi-x-square-fill"
+              width="1em"
+              height="1em"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2 0a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V2a2 2 0 00-2-2H2zm9.854 4.854a.5.5 0 00-.708-.708L8 7.293 4.854 4.146a.5.5 0 10-.708.708L7.293 8l-3.147 3.146a.5.5 0 00.708.708L8 8.707l3.146 3.147a.5.5 0 00.708-.708L8.707 8l3.147-3.146z"
+                clipRule="evenodd"
+              />
+            </svg>
           </span>
           <div className="row no-gutters">
             <div className="col-md-4">
-              <div className="header-card-show-image">
-                <img
-                  src={data.image.original}
-                  alt={data.name}
-                  className="img-thumbnail"
-                />
-              </div>
+              <div className="header-card-show-image"></div>
             </div>
             <div className="col-md-8">
               <div className="card-body" id="header-card-font">
