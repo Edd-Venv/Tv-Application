f8d3e2d7b3ad1c1b0378d493e18d5790db4e6c59
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-05-01 16:27:29 +0200

    Update

---
+++ server/controllers/UserSetUpControllers/UserSetUp.js
@@ -0,0 +1,141 @@
+const pool = require("../../database-connection/db.js");
+const { hash, compare } = require("bcryptjs");
+const { verify } = require("jsonwebtoken");
+const uuid_generate = require("uuid");
+const {
+  createAccessToken,
+  createRefreshToken,
+  sendRefreshToken,
+  sendAccessToken,
+} = require("../../src/tokens.js");
+
+exports.register = async (req, res) => {
+  const { person_name, password } = req.body;
+
+  try {
+    // 1. Check if the user exist
+    const checkDB = await pool.query(
+      `SELECT person_name FROM person WHERE person_name = '${person_name}'`
+    );
+    const doesUserExist = checkDB.rows[0];
+
+    if (doesUserExist !== undefined)
+      throw new Error(`USER NAME " ${person_name} " IS TAKEN.`);
+
+    // 2. If not user exist already, hash the password
+    const hashedPassword = await hash(password, 10);
+
+    // 3. Insert the user in database
+    await pool.query(
+      "INSERT INTO person (id_uid, person_name, password) VALUES($1, $2, $3)",
+      [uuid_generate.v4(), person_name, hashedPassword]
+    );
+    res.status(201).send({ status: "success", message: "User Created" });
+  } catch (err) {
+    res.status(404).send({ status: "error", error: `${err.message}` });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { person_name, password } = req.body;
+
+  try {
+    // 1. Check if the user exists
+    const checkDB = await pool.query(
+      `SELECT * from person where person_name = '${person_name}'`
+    );
+    const doesUserExist = checkDB.rows[0];
+
+    if (!doesUserExist)
+      throw new Error(`" ${person_name} " IS AN INVALID USER! `);
+
+    // 2. Compare crypted password and see if it checks out. Send error if not
+    const user = doesUserExist;
+
+    const valid = await compare(password, user.password);
+    if (!valid) throw new Error(" PASSWORD ENTERED IS NOT CORRECT! ");
+
+    // 3. Create Refresh- and Accesstoken
+    const accesstoken = createAccessToken(user.id_uid);
+    const refreshtoken = createRefreshToken(user.id_uid);
+
+    // 4. Store Refreshtoken with user in db
+    await pool.query(
+      `UPDATE person SET refreshtoken = '${refreshtoken}'
+             WHERE id_uid = '${user.id_uid}'`
+    );
+
+    // 5. Send token. Refreshtoken as a cookie and accesstoken as a regular response
+    sendRefreshToken(res, refreshtoken);
+    sendAccessToken(res, req, accesstoken);
+  } catch (err) {
+    res.status(404).json({
+      status: "error",
+      error: `${err.message}`,
+    });
+  }
+};
+
+exports.logOut = async (_req, res) => {
+  res.clearCookie("refreshtoken", { path: "/refresh_token" });
+
+  // Logic here for to also remove refreshtoken from dataBase
+  const { person_name } = _req.body;
+  await pool.query(
+    `UPDATE person SET refreshtoken = '' WHERE person_name = '${person_name}'`
+  );
+
+  return res.status(200).json({
+    status: "success",
+    message: "Logged out",
+  });
+};
+
+exports.refreshToken = async (req, res) => {
+  const token = req.cookies.refreshtoken;
+
+  // If we don't have a token in our request ask to relogin I think
+  if (!token) return res.send({ error: "token doesn't exist" });
+  // We have a token, let's verify it!
+  let payload = null;
+  try {
+    payload = verify(token, "EDWINRULESEVENMORE");
+  } catch (err) {
+    return res.status(404).send({ status: "error", error: err });
+  }
+
+  try {
+    // token is valid, now check if user exists in dataBase
+    const checkDB = await pool.query(
+      `SELECT id_uid FROM person WHERE id_uid = '${payload.userId}'`
+    );
+    const doesUserExist = checkDB.rows[0].id_uid;
+    if (!doesUserExist) throw new Error("User Doesn't Exist In DataBase");
+
+    // user exists, check if refreshtoken exist on user in dataBase.
+    const checkRefreshToken = await pool.query(
+      `SELECT refreshtoken FROM person WHERE id_uid = '${payload.userId}'`
+    );
+
+    if (checkRefreshToken.rows[0].refreshtoken !== token)
+      throw new Error("Tokens Don't Match");
+
+    // token exist, create new Refresh And Accesstoken
+    const id_uid = doesUserExist;
+    const accesstoken = createAccessToken(id_uid);
+    const refreshtoken = createRefreshToken(id_uid);
+
+    //Update refreshtoken on user in dataBase
+    await pool.query(
+      `UPDATE person SET refreshtoken = '${refreshtoken}'
+             WHERE id_uid = '${id_uid}'`
+    );
+
+    //All Checks Out send new refreshtoken and accesstoken
+    sendRefreshToken(res, refreshtoken);
+
+    return res.status(200).json({ status: "success", accesstoken });
+  } catch (err) {
+    return res.status(404).json({ status: "success", message: err });
+  }
+};
--- server/routes/MoviesRoutes/ApiCalls/ApiCalls.js
+++ server/routes/MoviesRoutes/ApiCalls/ApiCalls.js
@@ -1,34 +1,6 @@
-const NodeCache = require("node-cache");
-const fetch = require("node-fetch");
-const Cache = new NodeCache();
 const express = require("express");
 const router = express.Router();
+const moviesController = require("../../../controllers/MoviesControllers/ApiCalls/ApiCalls.js");
 
-//FETCH MOVIE DATA
-const movieApi1 =
-  "https://www.omdbapi.com/?t=Bohemian Rhapsody&apikey=728de06e";
-const movieApi2 = "https://www.omdbapi.com/?t=A Star Is Born&apikey=728de06e";
-const movieApi3 =
-  "https://www.omdbapi.com/?t=Fantastic Beasts: The Crimes Of Grindelwald&apikey=728de06e";
-const movieApi4 =
-  "https://www.omdbapi.com/?t=Jurassic World: Fallen Kingdom&apikey=728de06e";
-router.get("/movieData", async (req, res) => {
-  try {
-    const exists = Cache.has("movieData");
-
-    if (exists) {
-      res.json({ data: Cache.get("movieData") });
-    } else {
-      const result1 = await (await fetch(movieApi1)).json();
-      const result2 = await (await fetch(movieApi2)).json();
-      const result3 = await (await fetch(movieApi3)).json();
-      const result4 = await (await fetch(movieApi4)).json();
-      finalResult = [result1, result2, result3, result4];
-      Cache.set("movieData", finalResult, 691200);
-      res.json({ data: finalResult });
-    }
-  } catch (error) {
-    res.json({ error: error });
-  }
-});
+router.get("/movieData", moviesController.getMovieApiData);
 module.exports = router;
