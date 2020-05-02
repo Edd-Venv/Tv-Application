a0169a58ea5e1443dc6c3b1c7f736688522f4be3
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-05-02 16:18:32 +0200

    Update

--- server/routes/MoviesRoutes/ApiCalls/ApiCalls.js
+++ server/routes/MoviesRoutes/ApiCalls/ApiCalls.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const router = express.Router();
 const moviesController = require("../../../controllers/MoviesControllers/ApiCalls/ApiCalls.js");
+
+const router = express.Router();
 
 router.get("/movieData", moviesController.getMovieApiData);
 module.exports = router;
