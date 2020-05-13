e522c4feac4f3145d293c81fc2903fd9f7bcd5a8
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-05-13 11:15:10 +0200

    Update

---
+++ server/controllers/UserSetUpControllers/email.js
@@ -0,0 +1,49 @@
+const nodemailer = require("nodemailer");
+const htmlToText = require("html-to-text");
+const pug = require("pug");
+
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.userName;
+    this.url = url;
+    this.from = "Edwin's Support Team <eushibantusupprt@gmail.com>";
+  }
+
+  newTransport() {
+    return nodemailer.createTransport({
+      service: "SendGrid",
+      auth: {
+        user: "apikey",
+        pass:
+          "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",
+      },
+    });
+  }
+
+  async send(template, subject) {
+    const html = pug.renderFile(
+      `${__dirname}/../../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html),
+    };
+
+    this.newTransport();
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendPasswordReset() {
+    await this.send("passwordReset", "Password Reset, Reset Within 10mins!");
+  }
+};
