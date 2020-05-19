33fd1adf44e4c81a9e8a21356b4e325d8b65d550
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-05-19 11:43:01 +0200

    Update

--- client/src/tvShows/components/Body/Body.test.js
+++ client/src/tvShows/components/Body/Body.test.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
+import { findByTestAttr } from "../../../../test/testUtils.js";
 import Body from "./Body.js";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
-const setUp = (state) => {
+const setUp = (props, state) => {
   return shallow(<Body {...props} />);
 };
-describe("BODY TEST", () => {});
+describe("BODY TEST", () => {
+  it("RENDERS WITHOUT ERROR", () => {
+    const wrapper = setUp();
+    const bodyComponent = findByTestAttr(wrapper, "body-component");
+    expect(bodyComponent.length).toBe(1);
+  });
+});
