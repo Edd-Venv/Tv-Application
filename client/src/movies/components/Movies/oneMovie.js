00b4b6eb3328cbec9a9860ec9d8b3be7def666ec
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-05-23 19:18:41 +0200

    Update

--- client/src/movies/components/Movies/oneMovie.js
+++ client/src/movies/components/Movies/oneMovie.js
@@ -1,20 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
-import "./Movies.css";
+import "./oneMovie.css";
 
 const OneMovie = (props) => {
   const { content, deleteMovie } = props;
   return (
-    <div className="my-movies-container">
+    <div className="one-movie-container">
       {content[0].map((info) => {
         return (
-          <div
-            className="card mb-3"
-            style={{ width: "60%" }}
-            key={info.movie_key}
-          >
+          <div className="one-movie-card card mb-3" key={info.movie_key}>
             <div className="row no-gutters">
-              <div style={{ width: "30%", marginLeft: "2.7%" }}>
+              <div className="one-movie-image-container">
                 <img
                   alt="loading"
                   src={info.movie_image}
@@ -27,43 +23,33 @@ const OneMovie = (props) => {
                   <h2 className="card-title" style={{ marginLeft: "2%" }}>
                     {info.movie_title}
                   </h2>
-                  <p
-                    className="card-text"
-                    style={{ fontSize: "1.8rem", width: "100%" }}
-                  >
-                    <strong>Summary : </strong>{" "}
-                    {info.movie_summary.substring(0, 80)}
-                  </p>
-                  <p
-                    className="card-text"
-                    style={{ fontSize: "1.8rem", width: "100%" }}
-                  >
-                    <strong>Premier Date : </strong>
-                    {info.movie_release}
-                  </p>
-                  <p
-                    className="card-text"
-                    style={{ fontSize: "1.8rem", width: "100%" }}
-                  >
-                    <strong>Rating : </strong>
-                    {info.movie_rating}
-                  </p>
-                  <p
-                    className="card-text"
-                    style={{ fontSize: "1.8rem", width: "100%" }}
-                  >
-                    <strong>Genres : </strong>
-                    {info.movie_genre}
-                  </p>
-                  <button
-                    className="btn btn-danger"
-                    onClick={deleteMovie.bind(this, [
-                      info.movie_title,
-                      info.movie_key,
-                    ])}
-                  >
-                    Delete
-                  </button>
+                  <div className="one-movie-card-font">
+                    <p className="card-text">
+                      <strong>Summary : </strong>{" "}
+                      {info.movie_summary.substring(0, 80)}
+                    </p>
+                    <p className="card-text">
+                      <strong>Premier Date : </strong>
+                      {info.movie_release}
+                    </p>
+                    <p className="card-text">
+                      <strong>Rating : </strong>
+                      {info.movie_rating}
+                    </p>
+                    <p className="card-text">
+                      <strong>Genres : </strong>
+                      {info.movie_genre}
+                    </p>
+                    <button
+                      className="btn btn-danger"
+                      onClick={deleteMovie.bind(this, [
+                        info.movie_title,
+                        info.movie_key,
+                      ])}
+                    >
+                      Delete
+                    </button>
+                  </div>
                 </div>
               </div>{" "}
             </div>
