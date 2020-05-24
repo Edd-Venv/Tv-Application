a2e7c7409f31a3968fa4f152d5e24200194ebca1
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-05-24 12:36:40 +0200

    Update

--- client/src/nav/Content/Content.js
+++ client/src/nav/Content/Content.js
@@ -2,9 +2,6 @@ import React from "react";
 import TvShows from "../../tvShowsApp.js";
 
 const Content = React.memo((props) => {
-  // Could have something here to check for the time when the accesstoken expires
-  // and then call the refresh_token endpoint to get a new accesstoken automatically
-
   if (props.loading) return <div>Loading ...</div>;
   return (
     <React.Fragment>
@@ -14,20 +11,3 @@ const Content = React.memo((props) => {
 });
 
 export default Content;
-
-/*
-import { Redirect } from "@reach/router";
-const Content = React.memo(() => {
-  // Could have something here to check for the time when the accesstoken expires
-  // and then call the refresh_token endpoint to get a new accesstoken automatically
-  const [user] = useContext(UserContext);
-  if (!user.accesstoken) return <Redirect from="" to="login" noThrow />;
-  console.log("Content Rendering");
-  return (
-    <React.Fragment>
-      <p>content</p>
-    </React.Fragment>
-  );
-});
-
-*/
