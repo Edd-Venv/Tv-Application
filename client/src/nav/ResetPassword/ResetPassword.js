b38736a5d31b700959002aa417c01af239c2073b
51169097 <51169097+Edd-Venv@users.noreply.github.com>
2020-05-24 12:37:28 +0200

    Update

--- client/src/nav/ResetPassword/ResetPassword.js
+++ client/src/nav/ResetPassword/ResetPassword.js
@@ -60,17 +60,6 @@ const ResetPassword = (props) => {
     "reset-password-form"
   );
 
-  useEffect(() => {
-    if (document.getElementById("reset-password-tool-tip")) {
-      const setToolTip = document.getElementById("reset-password-tool-tip");
-      if (state.message !== "") setToolTip.textContent = state.message;
-
-      setTimeout(() => {
-        setToolTip.textContent = "";
-      }, 3000);
-    }
-  }, [state.message]);
-
   return (
     <React.Fragment>
       <Navigation />
